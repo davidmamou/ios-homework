0690f5ac7ffa944f2ae493adfced2f7daeb4bf23
Eric Shorr <shorr.eric@gmail.com>
2017-11-01 12:25:38 +0200

    add hellogbye project

---
+++ November 1 2017/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Eric Shorr on 01/11/2017.
+//  Copyright © 2017 Developer Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+        
+        
+        
+    }
+}
+
